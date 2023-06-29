3d0d206c3533509fc307352df925ceba1a6a1050
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-06-29 20:20:44 -0300

    + - / X concluided!!1

--- calculator/src/components/caculator.jsx
+++ calculator/src/components/caculator.jsx
@@ -27,7 +27,9 @@ export default function calculator(props){
 
 
     const teclas = ['C', '()', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', 'א', '0', '.', '=']
+    const [previousOp, setPreviousOP] = props.useState(0)
     const [currentOp, setCurrentOP] = props.useState(0)
+    
  
     const displayCOP = (cop)=>{
         if(currentOp == '0'){
@@ -35,39 +37,95 @@ export default function calculator(props){
             setCurrentOP(cop)
         } else {
             setCurrentOP(currentOp + cop)
+            
         }
         
     }
 
+    const FNumber = (e)=>{
+        
+        displayCOP(e.target.value)
+    }
+
 
     const Fexpression = (e)=>{
-        switch (e){
+       
+        switch (e.target.value){
             case 'C':
+                
                 break
             case '()':
+                
                 break
             case '%':
+                
                 break
             case '/':
+                if(previousOp === 0){
+                    setPreviousOP(currentOp + ' /')
+                } else {
+                    let prv = previousOp.slice(0,-1)
+                    
+                    const tot = (parseFloat(prv) / parseFloat(currentOp))
+                    setPreviousOP(0)
+                    return setCurrentOP(tot)
+                }
+                
+                setCurrentOP(0)
+                
                 break
             case 'X':
+                if(previousOp === 0){
+                    setPreviousOP(currentOp + ' X')
+                } else {
+                    let prv = previousOp.slice(0,-1)
+                    
+                    const tot = (parseFloat(prv) * parseFloat(currentOp))
+                    setPreviousOP(0)
+                    return setCurrentOP(tot)
+                }
+                
+                setCurrentOP(0)
                 break
             case '-':
+                if(previousOp === 0){
+                    setPreviousOP(currentOp + ' -')
+                } else {
+                    let prv = previousOp.slice(0,-1)
+                    
+                    const tot = (parseFloat(prv) - parseFloat(currentOp))
+                    setPreviousOP(0)
+                    return setCurrentOP(tot)
+                }
+                
+                setCurrentOP(0)
                 break
             case '+':
+                if(previousOp === 0){
+                    setPreviousOP(currentOp + ' +')
+                } else {
+                    let prv = previousOp.slice(0,-1)
+                    
+                    const tot = (parseFloat(prv)+ parseFloat(currentOp))
+                    setPreviousOP(0)
+                    return setCurrentOP(tot)
+                }
+                
+                setCurrentOP(0)
                 break
             case '.':
                 break
             case '=':
-                break       
+                break  
+                
+            default:
+                break
         }
     }
 
-    const FNumber = (e)=>{
-        displayCOP(e.target.value)
-    }
+    
 
-    const teclasRender = (tc)=>{ /*renderiza os elementos do calc */
+    const teclasRender = (tc)=>{ 
         const a = tc.map((e)=>{
             let elements = <button onClick={(el)=>(isNaN(e) ? Fexpression(el) : FNumber(el))} value={e}>{e}</button>
             return elements
@@ -88,7 +146,7 @@ export default function calculator(props){
             <div id='calculator'>
                 <div id="display">
                     <p id='currentOP'>{currentOp}</p>
-                    <p id='previousOP'>0</p>
+                    <p id='previousOP'>{previousOp}</p>
 
                 </div>
                 <div id="teclas">
